87c228a21a422d9b69dec7fa3458df69f2845228
Vitalii Samofal <vitalii.samofal@softkit.dev>
2023-07-24 14:51:15 +0100

    created base crypto module

---
+++ libs/crypto/src/__tests__/crypto.spec.ts
@@ -0,0 +1,47 @@
+import {
+  generateRandomId,
+  generateRandomIdAsync,
+  generateRandomIdWithoutSpecialCharacters,
+  generateRandomNumber,
+  hashPassword,
+  verifyPassword,
+} from "../index";
+
+describe('crypto tests', () => {
+  it.each(['a'.repeat(32), 'a'.repeat(64), 'a'.repeat(128), 'a'.repeat(256)])(
+    'hash and compare',
+    async (password) => {
+      const hashedPassword = await hashPassword(password);
+      const isPasswordValid = await verifyPassword(password, hashedPassword);
+      expect(isPasswordValid).toBe(true);
+    },
+  );
+
+  it('get random number', () => {
+    const randomNumber = generateRandomNumber();
+    expect(randomNumber).toBeGreaterThan(0);
+    expect(randomNumber).toBeLessThan(1_000_000);
+  });
+
+  it('get random number with length', () => {
+    const randomNumber = generateRandomNumber(10);
+    expect(randomNumber).toBeGreaterThan(0);
+    expect(randomNumber).toBeLessThan(10_000_000_000);
+  });
+
+  it('get random string without special characters', () => {
+    const randomString = generateRandomIdWithoutSpecialCharacters();
+    expect(randomString).toHaveLength(11);
+    expect(randomString).toMatch(/^[\dA-Z]+$/);
+  });
+
+  it('get random string async', async () => {
+    const randomString = await generateRandomIdAsync();
+    expect(randomString).toHaveLength(21);
+  });
+
+  it('get random string sync', async () => {
+    const randomString = generateRandomId();
+    expect(randomString).toHaveLength(21);
+  });
+});
---
+++ libs/crypto/src/lib/crypto.ts
@@ -0,0 +1,32 @@
+import * as argon2 from 'argon2';
+import { customAlphabet, nanoid } from 'nanoid';
+
+const nanoidNoSpecialCharacters = customAlphabet(
+  '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+);
+
+const nanoidOnlyNumbers = customAlphabet('1234567890');
+
+export async function generateRandomIdAsync() {
+  return nanoid();
+}
+
+export function generateRandomId() {
+  return nanoid();
+}
+
+export function generateRandomIdWithoutSpecialCharacters(length = 11) {
+  return nanoidNoSpecialCharacters(length);
+}
+
+export function generateRandomNumber(length = 6): number {
+  return Number.parseInt(nanoidOnlyNumbers(length));
+}
+
+export function hashPassword(password: string) {
+  return argon2.hash(password);
+}
+
+export async function verifyPassword(password: string, hashedPassword: string) {
+  return argon2.verify(hashedPassword, password);
+}
