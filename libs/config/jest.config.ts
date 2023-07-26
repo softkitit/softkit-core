e470c87eaa85264f3b67e3b0c99a30ab191c2ca3
Vitalii Samofal <vitalii.samofal@softkit.dev>
2023-07-26 22:45:45 +0100

    fixed config lib eslint errors

--- libs/config/jest.config.ts
+++ libs/config/jest.config.ts
@@ -1,11 +1,10 @@
-/* eslint-disable */
 export default {
   displayName: 'config',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/config'
+  coverageDirectory: '../../coverage/libs/config',
 };
