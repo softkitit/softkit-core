723621251b629dd57729c5865fb6ae5469f5e6ef
Vitalii Samofal <vitalii.samofal@softkit.dev>
2023-09-11 05:07:16 +0100

    test(platform): decreased test coverage for platform app, because it's not needed for now

--- apps/platform/jest.config.ts
+++ apps/platform/jest.config.ts
@@ -3,6 +3,14 @@ export default {
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transformIgnorePatterns: ['/node_modules/(?!nest-typed-config)'],
+  coverageThreshold: {
+    global: {
+      lines: 80,
+      statements: 80,
+      functions: 80,
+      branches: 80,
+    },
+  },
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
