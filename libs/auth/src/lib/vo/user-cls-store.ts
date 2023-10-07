301d6720c1e102bd6c8ba4305aa26eb06856c119
Vitalii Samofal <vitalii.samofal@softkit.dev>
2023-10-07 02:18:50 +0100

    fix(auth): excluded unnecessary dependencies. Added auth header for user clsStore

--- libs/auth/src/lib/vo/user-cls-store.ts
+++ libs/auth/src/lib/vo/user-cls-store.ts
@@ -4,5 +4,6 @@ import { IJwtPayload } from './payload';
 export interface UserClsStore<T extends IJwtPayload> extends TenantClsStore {
   jwtPayload: T;
   userId: string;
+  authHeader: string;
   tenantId: string;
 }
