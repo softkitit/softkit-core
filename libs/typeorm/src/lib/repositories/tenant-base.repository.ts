12acee550ea2156b5caed9f110b7fb562b5b8a94
Vitalii Samofal <vitalii.samofal@softkit.dev>
2023-10-07 02:05:05 +0100

    fix(typeorm): changed default tenant entity index. Made cls service protected to have access in parent repos

--- libs/typeorm/src/lib/repositories/tenant-base.repository.ts
+++ libs/typeorm/src/lib/repositories/tenant-base.repository.ts
@@ -10,7 +10,7 @@ export abstract class BaseTenantRepository<
   protected constructor(
     et: EntityTarget<ENTITY>,
     dataSource: DataSource,
-    private clsService: ClsService<TenantClsStore>,
+    protected clsService: ClsService<TenantClsStore>,
   ) {
     super(et, dataSource);
   }
