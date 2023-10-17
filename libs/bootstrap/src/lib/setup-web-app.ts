ac0538e7efe659938e31b499e2c934de7c10ddff
Vitalii Samofal <vitalii.samofal@softkit.dev>
2023-10-17 19:58:07 +0100

    fix(validation): added trim and lowercase transformer

--- libs/bootstrap/src/lib/setup-web-app.ts
+++ libs/bootstrap/src/lib/setup-web-app.ts
@@ -53,6 +53,7 @@ export function setupGlobalFilters(
     new AnyExceptionFilter(httpAdapterHost),
     new OverrideDefaultNotFoundFilter(httpAdapterHost),
     new OverrideDefaultForbiddenExceptionFilter(httpAdapterHost),
+    // todo generalize
     new PostgresDbQueryFailedErrorFilter(httpAdapterHost),
     new HttpExceptionFilter(httpAdapterHost),
     new I18nValidationExceptionFilter({
