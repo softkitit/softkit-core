c403fb5dc5ba6923d755bd583d5f41fb8fc31f34
Vitalii Samofal <vitalii.samofal@softkit.dev>
2023-11-02 04:21:29 +0000

    fix(typeorm): added proper handling for tenant id resolution in a preset condition

--- libs/typeorm/src/lib/repositories/tenant-base.repository.ts
+++ libs/typeorm/src/lib/repositories/tenant-base.repository.ts
@@ -3,6 +3,7 @@ import { ClsService } from 'nestjs-cls';
 import { DataSource, EntityTarget, FindOptionsWhere } from 'typeorm';
 import { BaseTenantEntityHelper } from '../entities/tenant-entity-helper';
 import { TenantClsStore } from '../vo/tenant-base-cls-store';
+import { GeneralInternalServerException } from '@softkit/exceptions';
 
 export abstract class BaseTenantRepository<
   ENTITY extends BaseTenantEntityHelper,
@@ -18,9 +19,18 @@ export abstract class BaseTenantRepository<
   protected override presetDefaultWhereOptions<
     T extends FindOptionsWhere<ENTITY> | undefined,
   >(currentOptions: T): T {
+    const tenantId = this.clsService.get().tenantId;
+
+    if (!tenantId) {
+      throw new GeneralInternalServerException(
+        `TenantId is not set for the required tenant id repository, it's either not set in the request
+        or you are trying to use the repository outside of the request scope or someone trying to cheat`,
+      );
+    }
+
     return {
       // allow to override tenantId from user perspective
-      tenantId: this.clsService.get().tenantId,
+      tenantId: tenantId,
       ...currentOptions,
     };
   }
