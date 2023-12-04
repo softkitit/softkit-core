d51f67e150354cc169204e5a4acba80c88c2e3a5
Nikolay Kolodnitsky <nkolodnitsky@softkitit.com>
2023-12-04 19:17:46 +0200

    fix(auth): updated the refresh token guards to get current user data in endpoints

--- libs/auth/src/__tests__/app/controllers/refresh-token-auth.controller.ts
+++ libs/auth/src/__tests__/app/controllers/refresh-token-auth.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { SkipAuth } from '../../../lib/decorators/skip-auth.decorator';
 import { RefreshJwtAuthGuard } from '../../../lib/guards/refresh-jwt-auth.guard';
+import { CurrentUser } from '../../../lib/decorators/current-user.decorator';
+import { IRefreshTokenPayload } from '../../../lib/vo/payload';
 
 @Controller('refresh-auth')
 @SkipAuth()
@@ -9,5 +11,13 @@ export class RefreshTokenAuthController {
   @UseGuards(RefreshJwtAuthGuard)
   async refreshAuthGuard() {
     return 'hello';
+  }
+
+  @Get('/context-user')
+  @UseGuards(RefreshJwtAuthGuard)
+  async refreshAuthGuardContextUserFromPayload(
+    @CurrentUser() user: IRefreshTokenPayload,
+  ) {
+    return user;
   }
 }
