1632a25c1ecc03420c0bd5b05bee7c09a9e51b1c
Vitalii Samofal <vitalii.samofal@softkit.dev>
2024-01-31 12:45:12 +0000

    test(i18n): changed coverage rule to match existing state

--- libs/i18n/jest.config.ts
+++ libs/i18n/jest.config.ts
@@ -5,6 +5,14 @@ export default {
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
+  coverageThreshold: {
+    global: {
+      statements: 89,
+      branches: 75,
+      functions: 90,
+      lines: 89,
+    },
+  },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/libs/i18n',
 };
