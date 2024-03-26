13bce123cd00749aca75ee9860e7053d2073f047
Y Dev <vpopov@softkitit.com>
2024-03-26 14:07:49 +0200

    feat(resource-plugin): enhance bash script with detailed functions

--- libs/resource-plugin/src/__tests__/run-test.sh
+++ libs/resource-plugin/src/__tests__/run-test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
-
 trap handle_exit EXIT SIGINT SIGTERM
 
 readonly WORK_DIR=$(mktemp -d)
 readonly SCRIPT_DIR=$(dirname "$(realpath "$0")")
-readonly APP_FOLDER="platform"
-readonly FOLDER="$WORK_DIR/$APP_FOLDER"
+readonly FIRST_APP_NAME="platform"
+readonly SECOND_APP_NAME="dashboard"
+readonly FOLDER="$WORK_DIR/$FIRST_APP_NAME"
 readonly LIBS_DIR="$FOLDER/libs/random/src"
 
 if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
@@ -15,111 +15,258 @@ if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
     exit 1
 fi
 
-handle_exit() {      
+handle_exit() {
+    docker stop testdb &>/dev/null
+    docker rm testdb &>/dev/null
     rm -rf "$WORK_DIR"
-    docker stop testdb &> /dev/null
-    docker rm testdb &> /dev/null
     echo "Deleted temp working directory $WORK_DIR and removed docker container testdb"
     exit 1
 }
 
 kill_process_on_port() {
-    local port=$1
+    local port="${1:-3000}"
     local pid=$(lsof -t -i:"$port" -sTCP:LISTEN)
+
     if [ -n "$pid" ]; then
         echo "Killing process $pid using port $port"
         kill -9 $pid
+    else
+        echo "No process found using port $port."
     fi
 }
 
 set_directories() {
-    MIGRATIONS_DIR="$FOLDER/apps/$1/src/app/database/migrations"
-    CONTROLLERS_DIR="$FOLDER/apps/$1/src/app/controllers"
-    ASSETS_DIR="$FOLDER/apps/$1/src/app/assets"
+    local app_name="${1:-$FIRST_APP_NAME}"
+
+    MIGRATIONS_DIR="$FOLDER/apps/$app_name/src/app/database/migrations"
+    CONTROLLERS_DIR="$FOLDER/apps/$app_name/src/app/controllers"
+    ASSETS_DIR="$FOLDER/apps/$app_name/src/app/assets"
 }
 
 check_server() {
-    local port=$1
-    echo "Checking server on port $port..."
-    until nc -z localhost "$port"; do   
+    local port="${1:-3000}"
+    local app_name="${2:-$FIRST_APP_NAME}"
+    local health_endpoint="http://localhost:$port/api/$app_name/health"
+
+    echo "Checking $app_name app server on port $port..."
+    until nc -z localhost "$port"; do
         sleep 1
     done
-    echo "Server on port $port is ready"
+
+    echo "Port $port for $app_name app is open, checking health endpoint..."
+    until [ "$(curl -s -o /dev/null -w '%{http_code}' $health_endpoint)" == "200" ]; do
+        echo "Waiting for the $app_name server on port $port to become healthy..."
+        sleep 1
+    done
+
+    echo "$app_name server on port $port is healthy and ready"
 }
 
-start_docker() {
-    docker pull postgres:15.4-alpine || { echo "Failed to pull PostgreSQL image"; handle_exit;  }
-    docker run --name testdb -e POSTGRES_PASSWORD=postgres -p 2221:5432 -d postgres || { echo "Failed to run PostgreSQL container"; handle_exit;  }
+start_postgres_in_docker() {
+    docker run --name testdb -e POSTGRES_PASSWORD=postgres -p 2221:5432 -d postgres:15.4-alpine || {
+        echo "Failed to run PostgreSQL container"
+        handle_exit
+    }
+}
+
+clone_repo() {
+    local tag=$1
+    npx ts-node libs/resource-plugin/src/generators/boilerplate/utils/cli.ts boilerplate-generator --appFolder "$FOLDER" --companyName "random1" --tag "$tag" --repository "https://github.com/softkitit/softkit-nestjs-boilerplate.git"
+    cd "$FOLDER"
+}
+
+install_dependencies() {
+    yarn install
+    sleep 1
+}
+
+generate_lib() {
+    local name="${1:-random}"
+    local import_path="${2:-@test/source}"
+    local buildable="${3:-true}"
+
+    npx nx generate @softkit/resource-plugin:lib --name="$name" --linter="eslint" --unitTestRunner="jest" --buildable=$buildable --importPath="$import_path" --languages="en" --lintCommandName=""
+}
+
+generate_resource() {
+    local project_name="${1:-$FIRST_APP_NAME}"
+    local entity_name="${2:-invoice}"
+    local group_name="${3:-invoices}"
+    local tenant_base_entity="${4:-false}"
+    local generate_repository="${5:-true}"
+    local generate_service="${6:-true}"
+    local generate_controller="${7:-true}"
+    local base_path="${8:-api/$project_name}"
+    local entity_includes_id_field="${9:-true}"
+    local entity_includes_version_field="${10:-true}"
+
+    npx nx generate @softkit/resource-plugin:resource --projectName="$project_name" --entityName="$entity_name" --basePath="$base_path" --groupName="$group_name" --tenantBaseEntity=$tenant_base_entity --generateRepository=$generate_repository --generateService=$generate_service --generateController=$generate_controller --entityIncludesIdField=$entity_includes_id_field --entityIncludesVersionField=$entity_includes_version_field --lintCommandName=""
+}
+
+generate_service() {
+    local project_name="${1:-$FIRST_APP_NAME}"
+    local entity_name="${2:-invoice-new}"
+    local group_name="${3:-invoices-new}"
+    local repository_name="${4:-invoice-new}"
+    local service_name="${5:-invoice-new}"
+    local tenant_base_service="${6:-false}"
+
+    npx nx generate @softkit/resource-plugin:service --projectName="$project_name" --serviceName="$service_name" --repositoryName="$repository_name" --entityName="$entity_name" --groupName="$group_name" --tenantBaseService=$tenant_base_service --lintCommandName=""
+}
+
+generate_controller() {
+    local project_name="${1:-$FIRST_APP_NAME}"
+    local entity_name="${2:-invoice-new}"
+    local controller_name="${3:-invoice-new}"
+    local group_name="${4:-invoices-new}"
+    local repository_name="${5:-invoice-new}"
+    local service_name="${6:-invoice-new}"
+    local tenant_base_entity="${7:-false}"
+    local base_path="${8:-api/$project_name}"
+    local entity_includes_id_field="${9:-true}"
+    local entity_includes_version_field="${10:-true}"
+
+    npx nx generate @softkit/resource-plugin:controller --projectName="$project_name" --controllerName="$controller_name" --basePath="$base_path" --serviceName="$service_name" --entityName="$entity_name" --groupName="$group_name" --tenantBaseEntity=$tenant_base_entity --entityIncludesIdField=$entity_includes_id_field --entityIncludesVersionField=$entity_includes_version_field --lintCommandName=""
+}
+
+generate_http_client() {
+    local name="${1:-$FIRST_APP_NAME}"
+    local import_path="${2:-@$FIRST_APP_NAME/client}"
+    local directory="${3:-clients}"
+
+    npx nx generate @softkit/resource-plugin:http-client --name="$name" --importPath="$import_path" --directory="$directory" --lintCommandName=""
+}
+
+generate_app() {
+    local app_name="${1:-$SECOND_APP_NAME}"
+    local app_port="${2:-9000}"
+    local i18n="${3:-true}"
+
+    # Do not pass "lintCommandName" as an empty string. This can lead to unintended behavior and potential bugs.
+    npx nx generate @softkit/resource-plugin:app $app_name --languages="en" --i18n=$i18n --appPort=$app_port
+}
+
+generate_migrations() {
+    local app_name="${1:-$FIRST_APP_NAME}"
+    local migration_name="${1:-add-invoice-entities}"
+
+    yarn run migration:generate $app_name $migration_name
+    node "$SCRIPT_DIR/test-utils.js" -m "$MIGRATIONS_DIR"
+}
+
+generate_client() {
+    local app_name="${1:-$FIRST_APP_NAME}"
+
+    npx nx reset
+    set +e
+    npx nx run $app_name:generate-client
+    local status=$?
+    set -e
+
+    if [ $status -ne 0 ]; then
+        echo "Generation failed, retrying..."
+        npx nx reset
+        npx nx run $app_name:generate-client
+    fi
+}
+
+run_migrations() {
+    local app_name="${1:-$FIRST_APP_NAME}"
+    export MIGRATION_APP_NAME=$app_name
+    yarn run typeorm --dataSource=node_modules/@softkit/typeorm/src/lib/migration/migration-data-source.js migration:run
 }
 
 generate_code() {
-    npx ts-node libs/resource-plugin/src/generators/boilerplate/utils/cli.ts boilerplate-generator --appFolder "$FOLDER" --companyName "random1" --tag "0.0.1" --repository "https://github.com/softkitit/softkit-nestjs-boilerplate.git"
-    cd "$FOLDER"
-    yarn install
-    sleep 1
+    local tag=$1
 
-    npx nx generate @softkit/resource-plugin:lib --name="random"  --linter="eslint" --unitTestRunner="jest" --buildable=true --importPath="@test/source" --languages="en"
-    npx nx generate @softkit/resource-plugin:resource --projectName="platform" --entityName="invoice" --basePath="api/platform" --groupName="invoices" --tenantBaseEntity=false --generateRepository=true --generateService=true --generateController=true --entityIncludesIdField=true --entityIncludesVersionField=true
-    npx nx generate @softkit/resource-plugin:http-client --name="platform" --importPath="@platform/client" --directory="clients"
+    clone_repo "$tag"
+    install_dependencies
 
-    npx nx generate @softkit/resource-plugin:app dashboard --languages="en"
-    npx nx generate @softkit/resource-plugin:resource --projectName="dashboard" --entityName="invoice" --basePath="api/platform" --groupName="invoices" --tenantBaseEntity=false --generateRepository=true --generateService=true --generateController=true --entityIncludesIdField=true --entityIncludesVersionField=true
+    set_directories
+    node "$SCRIPT_DIR/test-utils.js" -a "$ASSETS_DIR"
+    run_migrations
+
+    generate_lib
+    generate_resource
+    generate_http_client
+
+    generate_app
+    generate_resource $SECOND_APP_NAME
 }
 
-run_tests() {
-    set_directories "platform"
-    node "$SCRIPT_DIR/test-utils.js" -c "$CONTROLLERS_DIR" -l "$LIBS_DIR" -a "$ASSETS_DIR"
+run_app() {
+    local app_name="${1:-$FIRST_APP_NAME}"
+    local port="${2:-3000}"
+    local pid_var_name="${3:-FIRST_APP_PID}"
+    local development="${4:-true}"
 
-    kill_process_on_port 3000
-    npx nx run platform:serve &
-    PLATFORM_PID=$!
-    check_server 3000
-    sleep 1
+    kill_process_on_port $port
+    if [ "$development" = true ]; then
+        export NESTJS_PROFILES=local
+    else
+        unset NESTJS_PROFILES
+    fi
 
-    npx nx reset
-    yarn nx run platform:generate-client
+    npx nx run $app_name:serve &
+    eval "$pid_var_name=$!"
+    check_server $port $app_name
+}
 
-    npx nx generate @softkit/resource-plugin:resource --projectName="platform" --entityName="invoice-new" --basePath="api/platform" --groupName="invoices-new" --tenantBaseEntity=false --generateRepository=true --generateService=false --generateController=false --entityIncludesIdField=true --entityIncludesVersionField=true
-    npx nx generate @softkit/resource-plugin:service --projectName="platform" --serviceName="invoice-new" --repositoryName="invoice-new" --entityName="invoice-new" --groupName="invoices-new" --tenantBaseService=false
-    npx nx generate @softkit/resource-plugin:controller --projectName="platform" --controllerName="invoice-new" --basePath="api/platform" --serviceName="invoice-new" --entityName="invoice-new" --groupName="invoices-new" --tenantBaseEntity=false --entityIncludesIdField=true --entityIncludesVersionField=true
-    yarn run migration:generate platform add-invoice-entities
-    node "$SCRIPT_DIR/test-utils.js" -m "$MIGRATIONS_DIR" -c "$CONTROLLERS_DIR"
+test_uppercase_endpoint() {
+    local app_name="${1:-$SECOND_APP_NAME}"
+    local base_path="${2:-api/$app_name/v1}"
+    local entity_name="${3:-invoice}"
+    local port="${4:-9000}"
 
-    set_directories "dashboard"
-    node "$SCRIPT_DIR/test-utils.js" -c "$CONTROLLERS_DIR" -a "$ASSETS_DIR"
+    local route="${entity_name}/uppercase"
+    local url="http://localhost:${port}/$base_path/$route"
+    local nx_serve_cmd="nx run platform:serve"
 
-    kill_process_on_port 9000
-    npx nx run dashboard:serve &
-    DASHBOARD_PID=$!
-
-    check_server 9000
-    npx nx reset
-    yarn nx run dashboard:generate-client
-    check_server 3000
-
-    response=$(curl -s http://localhost:9000/api/dashboard/v1/invoice/uppercase)
+    echo "Testing endpoint: $url"
+    local response=$(curl "$url")
 
     echo "$response" | grep -q '"TEST STRING"'
     if [ $? -eq 0 ]; then
-        echo "Test for invoice/uppercase endpoint Passed. Response: $response'"
-        else
-        echo "Test for invoice/uppercase endpoint Failed: Response does not contain 'TEST STRING'"
+        echo "Test for $url Passed. Response: $response'"
+        # set +euo
+        # pkill -f "$nx_serve_cmd"
+        # exit 0
+    else
+        echo "Test for $url Failed: Response does not contain 'TEST STRING'"
     fi
+}
 
+run_tests() {
+    set_directories
+    node "$SCRIPT_DIR/test-utils.js" -c "$CONTROLLERS_DIR" -l "$LIBS_DIR" -a "$ASSETS_DIR"
 
-    new_response=$(curl http://localhost:3000/api/platform/v1/invoice-new)
-    if echo "$new_response" | grep -q '"totalItems":0'; then
-        echo "Test for invoice-new endpoint Passed: Response: $new_response"
-        else
-        echo "Test for invoice-new endpoint Failed. Response: $new_response"
-    fi
+    generate_client
 
-    npx nx platform:test --all --codeCoverage --skip-nx-cache --parallel=false
+    generate_resource $FIRST_APP_NAME "invoice-new" "invoices-new" false true false false
+    generate_service
+    generate_controller
+
+    generate_migrations
+
+    set_directories "$SECOND_APP_NAME"
+    node "$SCRIPT_DIR/test-utils.js" -c "$CONTROLLERS_DIR" -a "$ASSETS_DIR"
+
+    # npx nx run platform:test --all --codeCoverage --skip-nx-cache --parallel=false
+
+    run_app
+    run_app "$SECOND_APP_NAME" 9000 "SECOND_APP_PID" false
+
+    test_uppercase_endpoint
+
+    wait $FIRST_APP_PID
+    wait $SECOND_APP_PID
 }
 
 main() {
-    start_docker
-    generate_code
+    local tag="${1:-latest}"
+
+    start_postgres_in_docker
+    generate_code "$tag"
     run_tests
 }
 
