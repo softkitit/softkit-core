ca5422b04b570d5307fffd79ca4e1fb19ea3f0e3
Vitalii Samofal <vitalii.samofal@softkit.dev>
2024-05-23 18:46:24 +0100

    fix(mail): temporary lover coverage threshold

--- libs/mail/jest.config.ts
+++ libs/mail/jest.config.ts
@@ -5,6 +5,15 @@ export default {
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+    },
+  },
+
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/libs/mail',
 };
