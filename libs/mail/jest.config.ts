b87384b89569d03e88f16a4fa90f81a73dae994a
Vitalii Samofal <vitalii.samofal@softkit.dev>
2024-05-28 17:08:22 +0100

    fix(mail): added more tests for sendgrid

--- libs/mail/jest.config.ts
+++ libs/mail/jest.config.ts
@@ -5,15 +5,6 @@ export default {
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
-  coverageThreshold: {
-    global: {
-      statements: 90,
-      branches: 90,
-      functions: 90,
-      lines: 90,
-    },
-  },
-
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/libs/mail',
 };
