572fa7120848241686cf541d176f63230c31bb05
Vitalii Samofal <vitalii.samofal@softkit.dev>
2024-06-11 00:21:21 +0100

    feat(docs): added basic terraform definition to deploy site

---
+++ apps/docs/terraform/locals.tf
@@ -0,0 +1,18 @@
+# website variables
+locals {
+  # Editable Variables
+
+  parent_zone_env = tomap({
+    "prod" : local.base_domain
+  })
+
+  # Non-Editable Variables
+  prod_env         = var.environment == "prod"
+  base_domain      = "softkit.dev"
+  notFoundPagePath = "/404.html"
+  parent_zone_name = lookup(local.parent_zone_env, var.environment, "${var.environment}.${local.base_domain}")
+  website_domain   = "docs.${local.parent_zone_env}"
+  www_website_domain = "www.${local.website_domain}"
+
+
+}
